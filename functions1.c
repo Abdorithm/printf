ada9af73b3481f87b99681ca1d078d7648afdf9a
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-06-26 08:23:20 +0200

    hotter fix

--- functions1.c
+++ functions1.c
@@ -26,8 +26,7 @@ int print_int(va_list args)
 			negative = 2, n++;
 		else
 			negative = 1;
-		len++;
-		n = -n;
+		len++, n = -n;
 	}
 	while (copy)
 		len++, copy /= 10;
@@ -39,14 +38,13 @@ int print_int(va_list args)
 	if (num == NULL)
 		return (-1);
 	for (i = size - 1; i >= 0; i--)
+		num[i] = zero + (n % 10), n /= 10;
+	if (negative)
 	{
-		num[i] = zero + (n % 10);
-		n /= 10;
-	}
-	if (negative == 1)
 		write(1, &neg, 1);
-	else if (negative == 2)
-		num[size - 1] = '8', write(1, &neg, 1);
+		if (negative == 2)
+			num[size - 1] = '8';
+	}
 	for (i = 0; i < size; i++)
 		write(1, &num[i], 1);
 	free(num);
