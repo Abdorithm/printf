736f678491ad13a34a9bb82e1d6b947bc403f348
raniaelhagin <raniahamada0308@gmail.com>
2023-06-27 14:16:27 +0200

    binary conversion handled

--- functions2.c
+++ functions2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_binary - converts unsinged int to binary
@@ -10,7 +9,7 @@
 int print_binary(va_list args)
 {
 	int i = 0, size = 0;
-	char zero = '0', null_op = '\0';
+	char zero = '0';
 	char *binary_num;
 	unsigned int num = va_arg(args, unsigned int), num_cp = num;
 
@@ -28,7 +27,7 @@ int print_binary(va_list args)
 		size++;
 		num_cp /= 2;
 	}
-	binary_num = (char *)malloc(sizeof(char) * size + 1);
+	binary_num = (char *)malloc(sizeof(char) * size);
 	if (binary_num == NULL)
 		return (-1);
 
@@ -45,7 +44,6 @@ int print_binary(va_list args)
 		write(1, &binary_num[i], 1);
 		i--;
 	}
-	write(1, &null_op, 1);
 
 	free(binary_num);
 	return (size);
